99469b70af27af8b0268fca5d86fef8a97a4a414
Student <s525761@ch140012m.nwmissouri.edu>
2016-11-18 13:11:10 -0600

    Added clickMe method in A

--- GIT-ICE-KTS/EhVC.swift
+++ GIT-ICE-KTS/EhVC.swift
@@ -15,11 +15,15 @@ class EhVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var testA: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ButtonA(sender: AnyObject) {
+        testA.text = "You are with A"
+    }
 
 }
 
